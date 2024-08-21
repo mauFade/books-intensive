f7816860b94d75b0bc3088a8ba97e697a800576a
Mauricio Cardoso <mauricio.cds00@gmail.com>
2024-08-21 16:36:14 -0300

    fix: fix handler

--- internal/web/handler.go
+++ internal/web/handler.go
@@ -44,6 +44,7 @@ func (h *BookHandler) CreateBook(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(book)
 }
